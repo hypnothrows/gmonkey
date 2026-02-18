799d118f7324f80873f51b9a530b29d92be03192
hypnothrows <hypnothrows@gmail.com>
2026-02-18 00:32:25 -0500

    Create replacements.js

---
+++ resources/replacements.js
@@ -0,0 +1,16 @@
+// Artist replacements
+
+const REPLACEMENTS = {
+    ARTIST: {
+        'Goddess Athalia': 'Goddess Athalia Trance Treats',
+        'Selene Rey': ['SeleneRey', 'selenerey'],
+        'Goddess Alexa': ['iAmGoddessAlexa', 'iamgoddessalexa'],
+        'Roxanne Miller': 'RoxanneMiller',
+        'Raven Kiss': ['raven_kiss'],
+        'Madison Stone': ['MissMadisonStone', 'missmadisonstone', 'Miss Madison Stone', 'Miss Madison'],
+        'Princess Brielle': ['Princess Brielle FinDom FemDom'],
+    },
+    TITLE: {
+        '\'': ['`', '’'],
+    },
+};
